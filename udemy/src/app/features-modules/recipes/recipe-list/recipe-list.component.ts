f309d26d9cbdc69de50040c815df8f753852a7c3
deebye <derekebye@gmail.com>
2022-10-22 00:26:59 -0400

    changes

--- udemy/src/app/features-modules/recipes/recipe-list/recipe-list.component.ts
+++ udemy/src/app/features-modules/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is a test','https://images.immediate.co.uk/production/volatile/sites/2/2019/05/Caramalised-pork-ribs-10376c4.jpg?quality=90&crop=14px,1640px,5942px,2556px&resize=556,505'),
+    new Recipe('A Test Recipe','This is a test','https://images.immediate.co.uk/production/volatile/sites/2/2019/05/Caramalised-pork-ribs-10376c4.jpg?quality=90&crop=14px,1640px,5942px,2556px&resize=556,505'),
+    new Recipe('A Test Recipe','This is a test','https://images.immediate.co.uk/production/volatile/sites/2/2019/05/Caramalised-pork-ribs-10376c4.jpg?quality=90&crop=14px,1640px,5942px,2556px&resize=556,505')
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
